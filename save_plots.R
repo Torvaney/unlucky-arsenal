eba55b1ce125b089346343ef3a94620cd35623c1
Torvaney <btorvaney@gmail.com>
2017-03-26 16:42:33 +0100

    Move plots to new subdirectory

--- save_plots.R
+++ save_plots.R
@@ -6,5 +6,5 @@ for (team in unique(sim_positions$name)) {
   plot <- plot_positions(sim_positions, team, "gray20", "black") +
     ggtitle(team)
   
-  ggsave(paste0("figures/", team, ".png"), width = 20, height = 20, units = "cm")
+  ggsave(paste0("figures/team-plots/", team, ".png"), width = 20, height = 20, units = "cm")
 }
